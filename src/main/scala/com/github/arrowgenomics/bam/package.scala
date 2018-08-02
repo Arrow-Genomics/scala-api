55172e05b3df95b0e8102f0ad888a317d32861d1
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-08-02 15:02:48 +0200

    Adding methods

--- src/main/scala/com/github/arrowgenomics/bam/package.scala
+++ src/main/scala/com/github/arrowgenomics/bam/package.scala
@@ -3,7 +3,24 @@ package com.github.arrowgenomics
 import java.io.File
 
 package object bam {
-  def loadBam(file: File) = ???
+  type Field
 
-  def loadBamRegion(file: File, contig: String, start: Int, stop: Int) = ???
+  type Result
+
+  def loadBam(file: File): Result = ???
+
+  def loadBamFields(file: File, fields: List[Field]): Result = ???
+
+  def loadBamRegion(file: File, contig: String, start: Int, stop: Int): Result = ???
+
+  def loadBamRegionField(file: File, contig: String, start: Int, stop: Int, fields: List[Field]): Result = ???
+
+  def loadBamBlocks(file: File, start: Long, stop: Long): Result = ???
+
+  def loadBamBlocksField(file: File, start: Long, stop: Long, fields: List[Field]): Result = ???
+
+  def writeBam(data: Result, file: File): Unit = ???
+
+  def filterBam(data:Result): Result = ???
+
 }
